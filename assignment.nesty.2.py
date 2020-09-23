0e6a660cd27447f5348236a5bddb95e3fab2e9e5
Pinky-cloud224 <pinkydebnath4@gmail.com>
2020-09-23 13:16:03 +0600

    First Commit

---
+++ assignment.nesty.2.py
@@ -0,0 +1,43 @@
+#Assignment-2:
+a=23
+b=12
+c=90
+d=56
+e=0.9
+d=7.8
+f=0.00987
+
+if a>b>c>d>f or d>e>a>=b:
+    print("First Commit")
+if a>=b and d>=e:
+    print("Second Commit")
+if a==b or a!=b:
+    print("Third Commit")
+elif d>e>f:
+    print("Forth Commit")
+if c==d:
+    print("Fifth Commit")
+elif c%d==0:
+    print("Sixth Commmit")
+elif a>=c or e>=d and b<=e and d<a and a==f:
+    print("Seventh Commit")
+else:
+    print("First Else")
+if f!=c:
+    print("Eight Commit")
+if a>b==d:
+    print("Nine Commit")
+elif d==a or e<d:
+    print("Ten Commit")
+elif b%c==0:
+    print("Eleven Commit")
+elif d<e<f:
+    print("Twelve Commit")
+elif f!=e and a>b:
+    print("Thirten Commit")
+else:
+    print("Second Else")
+if c>=a and c>=e>f:
+    print("Forteen Commit")
+
+print("The End")
