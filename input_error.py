178aadc0102530486acccf14a526023309704551
Pinky-cloud224 <pinkydebnath4@gmail.com>
2020-10-08 12:48:19 +0600

    First Commit

---
+++ input_error.py
@@ -0,0 +1,46 @@
+a=input()
+b=input()
+c=input()
+d=input()
+e=input()
+
+if a and b and c and d and e:
+    print("First")
+elif a or b and c:
+    print("Second")
+elif a and b or c and d and e:
+    print("Third")
+else:
+    print("Something")
+
+##############################################
+
+a=int(input())
+b=int(input())
+c=int(input())
+d=float(input())
+e=int(input())
+
+if a and b and c and d and e:
+    print("First")
+elif a or b and c:
+    print("Second")
+elif a and b or c and d and e:
+    print("Third")
+else:
+    print("Something")
+#####################################################
+a=input("True")
+b=input("False")
+c=input("False")
+d=input("True")
+e=input("False")
+
+if a and b and c and d and e:
+    print("First")
+elif a or b and c:
+    print("Second")
+elif a and b or c and d and e:
+    print("Third")
+else:
+    print("Something")
